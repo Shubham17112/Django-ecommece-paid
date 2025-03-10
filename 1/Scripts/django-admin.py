255451e4f1d212660ae85f1b692837ac61f1283a
Shubham <personal17122004@gmail.com>
2025-03-10 21:52:37 +0530

    sf

---
+++ 1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\Users\Acer\Desktop\Django-Ecommerce-master\Django-Ecommerce-master\1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ demo/vercel_app.py
@@ -0,0 +1,3 @@
+from demo.wsgi import application  # Replace "myproject" with your project name
+
+app = application
